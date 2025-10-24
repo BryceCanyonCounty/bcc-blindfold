0a4d87485c2a73c7640f0b3d2e56eba8f9388349
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-10-23 21:50:27 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,20 +1,56 @@
 Config = {
+
+    devMode = {
+        active = true
+    },
+    -----------------------------------------------------
+
     escape = {
-        active = true,
-        lotonumb = {
-            5, 6, 20 -- List of numbers between 0-5000 that when matched will remove a players blindfold on themselves (more numbers  makes it easier to take off a blindfold from yourself)
-        },
-        button = 0x760A9C6F,
+        active = true,          -- Set to false to disable escape mechanic
+        maxRange = 5000,        -- Max range number for escape mechanic (higher number = harder to escape)
+        numList = { 5, 6, 20 }, -- Numbers that will allow escape when randomly selected (between 0 and maxRange / more numbers = easier to escape)
+        button = 0x760A9C6F,    -- 'G' key by default
         lang = {
             escape = 'Attempt to Break Blindfold',
             button = 'G'
         }
     },
+    -----------------------------------------------------
+
     lang = {
-        noplayers = "No players nearby",
-        noitem = "You are out of blindfolds"
+        noPlayers = 'No players nearby',
+        noItem = 'You are out of blindfolds'
     },
-    blindfoldcommand = true,
-    blindfoldseldcommand = true,
-    blindfolditem = true
+    -----------------------------------------------------
+
+    commands = {
+        blindPlayer = {
+            active = true,     -- Set false to disable command to blind nearest player
+            name = 'blindfold' -- Command name to blind nearest player
+        },
+
+        unBlindPlayer = {
+            name = 'unblindfold' -- Command name to unblind nearest player (always active)
+        },
+
+        blindSelf = {
+            active = true,       -- Set false to disable commands to blind and unblind yourself
+            name = 'blindfoldme' -- Command name to blind yourself
+        },
+        unBlindSelf = {
+            name = 'unblindfoldme' -- Command name to unblind yourself
+        }
+    },
+    -----------------------------------------------------
+
+    blindfoldItem = {
+        active = true,         -- Set to true to use a blindfold item from inventory to blind players
+        itemName = 'blindfold' -- Name of the blindfold item in database
+    },
+    -----------------------------------------------------
+
+    -- Database seeding control. When true the resource will attempt to
+    -- insert/update required items on resource start. Set to false to
+    -- prevent automatic seeding and use the console commands instead.
+    autoSeedDatabase = true
 }
