2b6565cdf78b05c89e267c2c5a396f7e27b3447e
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-10-23 21:50:45 -0700

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,49 +1,76 @@
-local VORPcore = {}
-TriggerEvent("getCore", function(core)
-	VORPcore = core
-end)
-
-local VORPInv =  exports.vorp_inventory:vorp_inventoryApi()
+local Core = exports.vorp_core:GetCore()
+local BccUtils = exports['bcc-utils'].initiate()
+---@type BCCBlindfoldDebugLib
+local DBG = BCCBlindfoldDebug
 
 
-RegisterNetEvent('bccblindfold:toggleblindfold')
-AddEventHandler('bccblindfold:toggleblindfold', function(player, toggle)
-	local _source = source
+RegisterNetEvent('bcc-blindfold:ManageBlindfold', function(player, toggle)
+	local src = source
+    local user = Core.getUser(src)
+
+	if not user then
+		DBG.Error('No user found for source: ' .. tostring(src))
+		return
+	end
+
 	local passed = true
+	local shouldConsume = false
+    local blindfoldItem = Config.blindfoldItem.itemName or 'blindfold'
 	if player ~= nil then
-		if Config.blindfolditem and toggle == true then
-			local itemCount = VORPInv.getItemCount(_source, 'blindfold')
+		-- If using an item, check source player's inventory 
+		if Config.blindfoldItem.active and toggle == true then
+			local itemCount = exports.vorp_inventory:getItemCount(src, nil, blindfoldItem)
+			DBG.Info('Inventory check src=' .. tostring(src) .. ' item=' .. tostring(blindfoldItem) .. ' count=' .. tostring(itemCount))
 			if itemCount > 0 then
-				VORPInv.subItem(_source, 'blindfold', 1)
+				shouldConsume = true
 			else
 				passed = false
-				VORPcore.NotifyRightTip(_source, Config.lang.noitem,4000)
+				Core.NotifyRightTip(src, Config.lang.noItem, 4000)
 			end
 		end
 
 		if passed then
 			if player == 'self' then
-				player = source
+				player = src
+			elseif type(player) == 'string' then
+				local asNum = tonumber(player)
+				if asNum then
+					player = asNum
+				end
 			end
-		
-			local Character = VORPcore.getUser(player).getUsedCharacter
-		
-			local skin = json.decode(Character.skin)
-			local comps = Character.comps
-			local playerSex = string.gsub(tostring(skin["sex"]), "mp_", "")
-		
-			TriggerClientEvent('bccblindfold:togblindfold', player, playerSex, comps, toggle)
+
+			local targetUser = Core.getUser(player)
+			if not targetUser then
+				DBG.Error('No target user for player=' .. tostring(player) .. ' (src=' .. tostring(src) .. ')')
+				Core.NotifyRightTip(src, Config.lang.noPlayers, 4000)
+				return
+			end
+
+			local character = targetUser.getUsedCharacter
+			local skin = json.decode(character.skin)
+			local comps = character.comps
+			local playerSex = string.gsub(tostring(skin['sex']), 'mp_', '')
+
+			DBG.Info('Triggering SetBlindfold for player=' .. tostring(player) .. ' sex=' .. tostring(playerSex) .. ' toggle=' .. tostring(toggle) .. ' (src=' .. tostring(src) .. ')')
+			TriggerClientEvent('bcc-blindfold:SetBlindfold', player, playerSex, comps, toggle)
+
+			-- consume the blindfold from the source only after successfully triggering
+			if shouldConsume then
+				exports.vorp_inventory:subItem(src, blindfoldItem, 1)
+				DBG.Info('Consumed item ' .. tostring(blindfoldItem) .. ' from src=' .. tostring(src))
+			end
 		end
 	else
-		VORPcore.NotifyRightTip(_source, Config.lang.noplayers,4000)
+		Core.NotifyRightTip(src, Config.lang.noPlayers, 4000)
 	end
 end)
 
-if Config.blindfolditem then
-	-- Facemask Usable Item
-	VORPInv.RegisterUsableItem('blindfold', function(data)
-		-- VORPInv.subItem(data.source, 'blindfold', 1)
-		TriggerClientEvent("bccblindfold:blindfolditem", data.source)
-		VORPInv.CloseInv(data.source)
-	end)		
+if Config.blindfoldItem.active then
+	-- Register blindfold item as usable from inventory
+	exports.vorp_inventory:registerUsableItem(Config.blindfoldItem.itemName, function(data)
+		TriggerClientEvent('bcc-blindfold:GetPlayerToBlindfold', data.source)
+		exports.vorp_inventory:closeInventory(data.source)
+	end)
 end
+
+BccUtils.Versioner.checkFile(GetCurrentResourceName(), 'https://github.com/BryceCanyonCounty/bcc-blindfold')
